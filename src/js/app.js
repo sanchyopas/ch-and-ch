d32c28223f5c89aafc1266a857d62018cf73cfc8
sanchyopas <saniagolovanev@gmail.com>
2024-11-14 11:54:31 +0700

    fix: infite logos scroll

--- src/js/app.js
+++ src/js/app.js
@@ -60,19 +60,24 @@ document.querySelectorAll('[data-anchor]').forEach(button => {
 const openPopup = (event) => {
   let popupBtn = event.target.closest('[data-popup]')
   if (popupBtn) {
-    console.log(popupBtn.dataset.popup);
-
-    let popup = document.getElementById(popupBtn.dataset.popup);
+    const popup = document.getElementById(popupBtn.dataset.popup);
     document.documentElement.classList.add('popup-show');
     popup.classList.add('popup_show');
+
+    let nameOrder = popupBtn.dataset.order;
+
+    if (nameOrder) {
+      let fieledHidden = popup.querySelector('input[name="service"]');
+      console.log(fieledHidden);
+      fieledHidden.value = nameOrder;
+    }
+
     bodyLock();
   }
 }
 
 const closePopup = (event) => {
   let popupCloseBtn = event.target.closest('.popup__close');
-  console.log(popupCloseBtn);
-
   if (popupCloseBtn) {
     const popup = popupCloseBtn.closest('.popup');
     popup.classList.remove('popup_show');
@@ -80,6 +85,25 @@ const closePopup = (event) => {
     bodyUnLock();
   }
 }
+
+const popup = document.querySelectorAll('.popup');
+if (popup) {
+  popup.forEach(popup => popup.addEventListener('click', (e) => {
+    if (!e.target.closest('.popup__content')) {
+      e.currentTarget.classList.remove('popup_show');
+      document.documentElement.classList.remove('popup-show');
+      bodyUnLock();
+    }
+  }))
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 27 && document.querySelector('.popup_show').classList.contains('popup_show')) {
+    document.querySelector('.popup_show').classList.remove('popup_show');
+    document.documentElement.classList.remove('popup-show');
+    bodyUnLock();
+  }
+})
 
 const popupBtn = document.querySelectorAll('[data-popup]');
 if (popupBtn) {
